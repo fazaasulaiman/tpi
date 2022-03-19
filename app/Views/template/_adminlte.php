719fb86accbb52524beba9357652b6871b147b32
faza <fazaasulaiman@gmail.com>
2022-03-19 21:35:12 +0800

    fixing template

--- app/Views/template/_adminlte.php
+++ app/Views/template/_adminlte.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | DataTables</title>
+  <title>TPI| Ngurah Rai</title>
   
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -48,7 +48,7 @@
     <!-- Brand Logo -->
     <a href="/lte/index3.html" class="brand-link">
       <img src="/lte/assets/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">TPI NGR</span>
     </a>
 
     <!-- Sidebar -->
@@ -91,13 +91,13 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="../tables/simple.html" class="nav-link">
+                <a href="/lapharian/tambah" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Tambah</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="../tables/data.html" class="nav-link active">
+                <a href="/lapharian" class="nav-link active">
                   <i class="far fa-circle nav-icon"></i>
                   <p>List Harian</p>
                 </a>
@@ -119,9 +119,9 @@
   <!-- /.content-wrapper -->
   <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.2.0
+      <b>Version</b> 0.0.0.0.1
     </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2022 <a href="https://adminlte.io">TPI NGR</a>.</strong> All rights reserved.
   </footer>
 
   <!-- Control Sidebar -->
